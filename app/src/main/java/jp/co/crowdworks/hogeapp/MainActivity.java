ceddf04d38a0f22171f5b32c1f7ca6ccec30790f
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2015-04-02 20:47:51 +0900

    ベース。

---
+++ app/src/main/java/jp/co/crowdworks/hogeapp/MainActivity.java
@@ -0,0 +1,74 @@
+package jp.co.crowdworks.hogeapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnTest1 = (Button) findViewById(R.id.activity_main_btn_test1);
+        btnTest1.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int id = v.getId();
+        TextView txt = new TextView(this);
+        txt.setText("This application will be quited. Are you sure?");
+        if (R.id.activity_main_btn_test1 == id) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Really Exit?")
+                    .setView(txt)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getApplicationContext(), "canceled", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    })
+                    .create().show();
+        }
+    }
+}
